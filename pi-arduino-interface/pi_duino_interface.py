d8203bd723e18a4137c94a72795eb8df7ccac3e1
nishankjain <nishankjain@users.noreply.github.com>
2018-11-09 14:00:06 -0500

    Odometry working code.

---
+++ pi-arduino-interface/pi_duino_interface.py
@@ -0,0 +1,20 @@
+#!/user/bin/env python
+import serial
+port = "/dev/ttyACM0"
+rate = 9600
+
+s1 = serial.Serial(port, rate)
+s1.flushInput()
+
+comp_list = ["Flash complete\r\n", "Hello Pi, this is Arduino UNO...\r\n"]
+while True:
+    if s1.inWaiting() > 0:
+        inputValue = s1.readLine()
+        print(inputValue)
+        if inputValue in comp_list:
+            try:
+                n = input("Set Arduino flash times:")
+                s1.write('&d', %n)
+            except:
+                print("Input error, please input a number")
+                s1.write('0')
