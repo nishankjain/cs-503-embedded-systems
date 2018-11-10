a55eec79d2b1c610d85a5b35b8b8ba40589d64df
nishankjain <nishankjain@users.noreply.github.com>
2018-11-10 12:46:55 -0500

    Working code for odometry.

--- odometry/twoMotorOdometry/twoMotorOdometry.ino
+++ odometry/twoMotorOdometry/twoMotorOdometry.ino
@@ -6,71 +6,71 @@ volatile long enc_count2 = 0;
 
 void stopIfFault()
 {
- if (md.getFault())
- {
-   Serial.println("fault");
-   while(1);
- }
+if (md.getFault())
+{
+  Serial.println("fault");
+  while(1);
+}
 }
 
 void setup()
 {
- Serial.begin(115200);
- attachInterrupt(0,encoder_isr,CHANGE);
- attachInterrupt(1,encoder_isr2,CHANGE);
- Serial.println("Dual MC33926 Motor Shield");
- md.init();
+Serial.begin(115200);
+attachInterrupt(0,encoder_isr,CHANGE);
+attachInterrupt(1,encoder_isr2,CHANGE);
+Serial.println("Dual MC33926 Motor Shield");
+md.init();
 }
 
 void encoder_isr() {
-  static int8_t lookup_table[] = {0,0,0,1,0,0,-1,0,0,-1,0,0,1,0,0,0};
-  static uint8_t enc_val1 = 0;
+ static int8_t lookup_table[] = {0,0,0,1,0,0,-1,0,0,-1,0,0,1,0,0,0};
+ static uint8_t enc_val1 = 0;
 
-  //pin 2
-  uint8_t interrupt = (PIND & 0b100) >> 2;
+ //pin 2
+ uint8_t interrupt = (PIND & 0b100) >> 2;
 
-  //pin 6
-  uint8_t second = (PIND & 0b1000000) >> 5;
+ //pin 6
+ uint8_t second = (PIND & 0b1000000) >> 5;
 
-  enc_val1 = enc_val1 << 2;
-  enc_val1 = enc_val1 | (interrupt | second);
-  enc_count1 = enc_count1 + lookup_table[enc_val1 & 0b1111];
+ enc_val1 = enc_val1 << 2;
+ enc_val1 = enc_val1 | (interrupt | second);
+ enc_count1 = enc_count1 + lookup_table[enc_val1 & 0b1111];
 
-  Serial.print("MR: ");
-  Serial.println(enc_count1);
+ Serial.print("MR: ");
+ Serial.println(enc_count1);
 }
 
 void encoder_isr2() {
-  static int8_t lookup_table[] = {0,0,0,1,0,0,-1,0,0,-1,0,0,1,0,0,0};
-  static uint8_t enc_val2 = 0;
+ static int8_t lookup_table[] = {0,0,0,1,0,0,-1,0,0,-1,0,0,1,0,0,0};
+ static uint8_t enc_val2 = 0;
 
-  //pin 3
-  uint8_t interrupt = (PIND & 0b1000) >> 3;
-  //pin 5
-  uint8_t second = (PIND & 0b100000) >> 4;
+ //pin 3
+ uint8_t interrupt = (PIND & 0b1000) >> 3;
+ //pin 5
+ uint8_t second = (PIND & 0b100000) >> 4;
 
-  enc_val2 = enc_val2 << 2;
-  enc_val2 = enc_val2 | (interrupt | second);
-  enc_count2 = enc_count2 + lookup_table[enc_val2 & 0b1111];
+ enc_val2 = enc_val2 << 2;
+ enc_val2 = enc_val2 | (interrupt | second);
+ enc_count2 = enc_count2 + lookup_table[enc_val2 & 0b1111];
 
-  Serial.print("ML: ");
-  Serial.println(enc_count2);
+ Serial.print("ML: ");
+ Serial.println(enc_count2);
 }
 
-  void loop() {
-   unsigned long timeRunning = millis();
+ void loop() {
+  unsigned long timeRunning = millis();
 //   md.setM1Speed(150);  // Right Motor
 //   md.setM2Speed(150); // Left Motor
-   stopIfFault();
-   delay(1);
-   if (timeRunning > 3000) {
-    Serial.print("Time: ");
-    Serial.println(timeRunning);
-      md.setM1Speed(0);  // Right Motor
-      md.setM2Speed(0); // Left Motor
-   }
-   else {
-      md.setM1Speed(400);  // Right Motor
-      md.setM2Speed(400); // Left Motor
-   }
+  stopIfFault();
+  delay(1);
+  if (timeRunning > 3000) {
+   Serial.print("Time: ");
+   Serial.println(timeRunning);
+     md.setM1Speed(0);  // Right Motor
+     md.setM2Speed(0); // Left Motor
+  }
+  else {
+     md.setM1Speed(350);  // Right Motor
+     md.setM2Speed(350); // Left Motor
+  }
 }
--- pi-arduino-interface/pi_duino_interface.py
+++ pi-arduino-interface/pi_duino_interface.py
@@ -1,7 +1,8 @@
 #!/user/bin/env python
 import serial
 # port = "/dev/ttyACM0"
-port = "/dev/cu.usbmodem1431"
+# port = "/dev/cu.usbmodem1431"
+port = "COM7"
 rate = 115200
 
 s1 = serial.Serial(port, rate)
@@ -30,22 +31,62 @@ pwm: 275, velocity: 0.3448355756414529
 pwm: 300, velocity: 0.38302206556147955
 pwm: 400, velocity: 0.5508111879373542
 """
+
+"""
+pwm: 50
+Right: 0.04860098717094302
+Left: 0.03702932355881373
+
+pwm: 150
+Right: 0.2106042777407531
+Left: 0.21291861046317895
+
+Right: 0.1550086400732845
+Left: 0.1457543929047302
+
+pwm: 250
+Right: 0.37260756831056313
+Left: 0.3864935646451183
+
+Right: 0.310120584805065
+Left: 0.3147492502499167
+
+pwm: 350
+Right: 0.5506277065289807
+Left: 0.5367463357761493
+
+Right: 0.4651808772075975
+Left: 0.4651808772075975
+
+"""
+
+"""
+Right Motor Equation:
+vr = 0.0016pwm - 0.0776
+
+Left Motor Equation:
+vl = 0.0016pwm - 0.0907
+"""
+
 while True:
-    if s1.inWaiting() > 0:
-        inputValue = s1.readline().decode().strip('\r\n')
-        wheel = inputValue[1]
-        if inputValue[0:4] == "Time":
-            time = int(inputValue[6:])
-            break
-        if inputValue[0] == "M":
-            val = int(inputValue[4:])
-            if wheel == "R":
-                rightRotations = val
-            elif wheel == "L":
-                leftRotations = val
-        print("Right: %d, Left: %d" % (rightRotations, leftRotations))
+   if s1.inWaiting() > 0:
+       print(s1.readline())
+       inputValue = s1.readline().decode().strip('\r\n')
+       wheel = inputValue[1]
+       if inputValue[0:4] == "Time":
+           time = int(inputValue[6:])
+           break
+       if inputValue[0] == "M":
+           val = int(inputValue[4:])
+           if wheel == "R":
+               rightRotations = val
+           elif wheel == "L":
+               leftRotations = val
+       print("Right: %d, Left: %d" % (rightRotations, leftRotations))
 
 
-velocity = ((rightRotations + leftRotations)/2)*(circumfrence/32)/(time/1000)
+velocity_right = (rightRotations)*(circumfrence/32)/(time/1000)
+velocity_left = (leftRotations)*(circumfrence/32)/(time/1000)
 print(time)
-print(velocity)
+print(velocity_right)
+print(velocity_left)
